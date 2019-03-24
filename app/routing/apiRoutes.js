4ff38f9cc1fa0d135c750deed454e8f1fdf204c5
JJ <jenjones@ucdavis.edu>
2019-03-24 14:30:16 -0700

    html and api route files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,17 @@
 // Link routes to data source
+var friends = require("../data/friends");
 
-var friendsData = require("../data/friendsData");
+// ============== Routes ==============
+module.exports = function(app) {
 
+    // API GET request
+    app.get("/api/friends", function(req,res) {
+        res.json(friends);
+    });
+
+    // API POST Request
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
+        res.json(friends);
+    });
+}
