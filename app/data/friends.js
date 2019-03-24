662081cd05a8ec2b0f8cbe23493edb02f069db22
JJ <jenjones@ucdavis.edu>
2019-03-24 15:15:47 -0700

    dummy data in friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,198 @@
 // Data for friends
 
 var friendsArray = [
-{
-
-}
+    {
+        name: "Bob",
+        photo: "https://unsplash.com/photos/sibVwORYqs0",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Sue",
+        photo: "https://unsplash.com/photos/rDEOVtE7vOs",
+        scores: [
+            "1",
+            "5",
+            "2",
+            "3",
+            "1",
+            "5",
+            "4",
+            "1",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Charlie",
+        photo: "https://unsplash.com/photos/tAvpDE7fXgY",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Rose",
+        photo: "https://unsplash.com/photos/emxAxutgt-A",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Anne",
+        photo: "https://unsplash.com/photos/vYpbBtkDhNE",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Chris",
+        photo: "https://unsplash.com/photos/tHiGKAJxaA8",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Jack",
+        photo: "https://unsplash.com/photos/agGIKYs4mYs",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Margo",
+        photo: "https://unsplash.com/photos/mEZ3PoFGs_k",
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Theo",
+        photo: "https://unsplash.com/photos/wKOKidNT14w",
+        scores: [
+            "2",
+            "3",
+            "1",
+            "4",
+            "3",
+            "1",
+            "3",
+            "3",
+            "5",
+            "3"
+        ]
+    },
+    {
+        name: "Jessica",
+        photo: "https://unsplash.com/photos/W7b3eDUb_2I",
+        scores: [
+            "2",
+            "2",
+            "3",
+            "3",
+            "4",
+            "5",
+            "3",
+            "5",
+            "4",
+            "3"
+        ]
+    },
+    {
+        name: "Mike",
+        photo: "https://unsplash.com/photos/zF8ss0qB_ik",
+        scores: [
+            "1",
+            "2",
+            "1",
+            "2",
+            "3",
+            "3",
+            "2",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Chantelle",
+        photo: "https://unsplash.com/photos/Ugpcxb0jG4Q",
+        scores: [
+            "2",
+            "3",
+            "2",
+            "4",
+            "3",
+            "3",
+            "4",
+            "2",
+            "3",
+            "3"
+        ]
+    }
 ];
 
 module.exports = friendsArray;
